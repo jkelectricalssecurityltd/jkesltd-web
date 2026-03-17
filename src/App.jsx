c58b844adfd84fb214536e109d7aa48b4a2bc627
jkelectricalssecurityltd <contact@jkelectricalssecurityltd.co.uk>
2026-03-17 14:24:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import Home from "./pages/index";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages";           // index.jsx
+import About from "./pages/About";
+import CCTV from "./pages/CCTV";
+import Alarms from "./pages/Alarms";
+import PatTesting from "./pages/PatTesting";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cctv" element={<CCTV />} />
+        <Route path="/alarms" element={<Alarms />} />
+        <Route path="/pattesting" element={<PatTesting />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
